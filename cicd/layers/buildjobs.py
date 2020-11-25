37f0539394874d74a79f9f7b17c4738769768f12
natebachmeier <nate@bachmeier>
2020-11-25 00:14:37 -0500

    use nested stacks for python build jobs

--- cicd/layers/buildjobs.py
+++ cicd/layers/buildjobs.py
@@ -18,6 +18,25 @@ class BuildJobLayer(core.Construct):
   def __init__(self, scope: core.Construct, id: str, buckets:BucketLayer, **kwargs) -> None:
     super().__init__(scope, id, **kwargs)
 
+    self.build_role = iam.Role(self,'CodeBuildRole',
+      assumed_by= iam.ServicePrincipal(service="codebuild.amazonaws.com"),
+      description='FinSurf code building account',
+      managed_policies=[iam.ManagedPolicy.from_aws_managed_policy_name("AWSCodeBuildAdminAccess")])
+
+    BuildPythonZip(self,'Earnings-DataServices',
+      project_name='Earnings-DataServices',
+      buckets=buckets,
+      build_role=self.build_role,
+      app_dir='src/earnings',
+      output_name='earnings.zip')
+
+class BuildPythonZip(core.NestedStack):
+  """
+  Configure and deploy the network
+  """
+  def __init__(self, scope: core.Construct, id: str, project_name:str,buckets:BucketLayer, build_role:iam.Role,app_dir:str,output_name:str, **kwargs) -> None:
+    super().__init__(scope, id, **kwargs)
+
     self.github_master_source = b.Source.git_hub(
       clone_depth=1,
       owner='dr-natetorious',
@@ -25,36 +44,21 @@ class BuildJobLayer(core.Construct):
       webhook=False
     )
 
-    # self.github_private_source = b.Source.git_hub(
-    #   owner='dr-natetorious',
-    #   repo='app-FinSurf',
-    #   webhook=True,
-    #   webhook_filters=[
-    #     b.FilterGroup.in_event_of(b.EventAction.PUSH)
-    #     .and_branch_is_not('master')
-    #   ]
-    # )
-
-    self.build_role = iam.Role(self,'CodeBuildRole',
-      assumed_by= iam.ServicePrincipal(service="codebuild.amazonaws.com"),
-      description='FinSurf code building account',
-      managed_policies=[iam.ManagedPolicy.from_aws_managed_policy_name("AWSCodeBuildAdminAccess")])
-
-    self.earnings_project = b.Project(self,'Earnings',
-      project_name='Earnings-DataServices',
+    self.earnings_project = b.Project(self,'PythonProject',
+      project_name=project_name,
       source= self.github_master_source,
       environment= b.BuildEnvironment(
         build_image= b.LinuxBuildImage.from_docker_registry(name='python:3.8'),
         environment_variables={
-          'APP_DIR':b.BuildEnvironmentVariable(value='src/earnings')
+          'APP_DIR':b.BuildEnvironmentVariable(value=app_dir)
         },
         compute_type=b.ComputeType.SMALL
       ),
-      role=self.build_role,
+      role=build_role,
       encryption_key= buckets.artifacts_key,
       build_spec= b.BuildSpec.from_source_filename(filename='cicd/configs/buildspec-python-zip.yml'),
       artifacts= b.Artifacts.s3(
-        name="earnings.zip",
+        name=output_name,
         path="/artifacts",
         bucket=buckets.artifacts_bucket,
         encryption=True,
