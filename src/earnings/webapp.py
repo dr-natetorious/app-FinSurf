e0b85d3a2870fbc9d66467cbf152dc2f72a24e40
natebachmeier <nate@bachmeier>
2020-11-24 22:03:01 -0500

    add buildspec

--- src/earnings/webapp.py
+++ src/earnings/webapp.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from lib.datasources import WebEarningCalendar
+from json import dumps
+
 app = Flask(__name__)
+calendar = WebEarningCalendar()
 
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
 
-@app.route('/d/<date>')
-def fetch_date(date):
-  return 'Hello, World! - {d}'.format(d=date)
+@app.route('/d/<date_str>')
+def fetch_by_date(date_str):
+  return dumps(calendar.get_for_date(date_str=date_str))
