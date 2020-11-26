a05b18340b0f21f54306cecaf843d360065b8351
natebachmeier <nate@bachmeier>
2020-11-25 22:57:07 -0500

    this is starting to take form

--- src/alexa-skill/FetchEarningsByDate.py
+++ src/alexa-skill/FetchEarningsByDate.py
@@ -1,5 +1,7 @@
 import logging
-from json import dumps
+import os
+import requests
+from json import dumps, loads
 from ask_sdk_core.skill_builder import CustomSkillBuilder
 from ask_sdk_core.api_client import DefaultApiClient
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
@@ -20,9 +22,24 @@ class FetchEarningsByDateHandler(AbstractRequestHandler):
     return ask_utils.is_intent_name('FetchEarningsByDate')(handler_input)
 
   def handle(self, handler_input:HandlerInput) -> Response:
+    date = ask_utils.get_slot_value(handler_input,'date')
+
+    text = self.__process(date)
     return (
       handler_input.response_builder
-      .speak('let me check on that')
+      .speak(text)
       .response
     )
   
+  def __process(self, date):
+    resource_url = os.environ['EARNINGS_API']
+    if resource_url.endswith('/') == False:
+      resource_url+='/'
+    resource_url += date
+
+    df = loads(requests.get(resource_url).text)
+
+    return "There are {count} reportings including {highlights}".format(
+      count=len(df),
+      highlights=", ".join([x['symbol'] for x in df][0:3])
+    )
