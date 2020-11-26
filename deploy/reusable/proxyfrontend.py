883af2b4a0a2bcff2b0656ab72f5509d23eefc50
natebachmeier <nate@bachmeier>
2020-11-26 16:16:00 -0500

    adding fnapi to alexa

---
+++ deploy/reusable/proxyfrontend.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from reusable.context import InfraContext
+from aws_cdk import (
+  core,
+  aws_apigateway as a,
+  aws_lambda as lambda_,
+  aws_apigateway as a,
+  aws_route53 as dns,
+  aws_route53_targets as dns_t,
+  aws_certificatemanager as acm,
+)
+
+class LambdaProxyConstruct(core.Construct):
+  """
+  Configures a lambda behind APIGateway
+  """
+  def __init__(self, scope: core.Construct, id: str,
+    handler:lambda_.Function,
+    context:InfraContext, **kwargs) -> None:
+    super().__init__(scope, id, **kwargs)
+
+    self.rest_api = a.LambdaRestApi(self,id,
+      options=a.RestApiProps(),
+      handler=handler,
+      proxy=True,
+      description='Frontend proxy for '+handler.function_name)
