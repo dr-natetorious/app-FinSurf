e64b66992e696028247661b17f1c023afd844e5b
natebachmeier <nate@bachmeier>
2020-11-28 02:59:56 -0500

    write the script

--- src/portfolio-mgmt/fundamentals/main.py
+++ src/portfolio-mgmt/fundamentals/main.py
@@ -1,59 +1,59 @@
 #!/usr/bin/env python3
-
 from ClientFactory import ClientFactory
 from os import environ
 from json import dumps
+from logging import getLogger
 
+
+logger = getLogger()
 factory = ClientFactory()
 tdclient = factory.create_client()
-streamer = tdclient.create_streaming_session()
 
-def include_default_counters():
+def process_all_instruments():  
   """
-  Include default symbols...
+  Enumerates through all symbols
   """
-  streamer.level_one_futures(symbols=['/ES','/VX','/GC','/CL','/ZB','/ZN','/NG','/HG','/ZS','/ZO','/ZW','/6E','/6B','/RTY'],fields=[0,1,2,3,4,8])
-  streamer.account_activity()
+  for prefix in range(65,91):
+    prefix = chr(prefix)+'.*'
 
-def include_positions():
-  """
-  Find portfolio positions...
-  """
-  call_options = []
-  put_options = []
-  equities = ['$TICK','$UVOL','$DVOL','$ADVN','$DECN','$TRIN','$ADD']
-  accounts = tdclient.get_accounts(fields=['positions'])
-  print("Init for {} accounts".format(len(accounts)))
-  for account in accounts:
-    securitiesAccount = account['securitiesAccount']
-    if 'positions' not in securitiesAccount:
-      continue
+    instruments = tdclient.search_instruments(
+      symbol=prefix,
+      projection='symbol-regex')
+
+    logger.debug('Query Prefix {} found {} instruments...'.format(
+      prefix, len(instruments)))
     
-    positions = securitiesAccount['positions']
-    print('Found {} positions'.format(len(positions)))
-    for position in positions:
-      instrument = position['instrument']
-      assetType = instrument['assetType']
-      symbol = instrument['symbol']
-      print(assetType +' '+symbol)
-      
-      if assetType == 'EQUITY':
-        equities.append(symbol)
-      if assetType == 'OPTION':
-        equities.append(instrument['underlyingSymbol'])
-        putCalls = instrument['putCall']
-        if putCalls == 'CALL':
-          call_options.append(symbol)
-        else:
-          put_options.append(symbol)
-  
-  equities = list(set(equities))
-  print(equities)
-  streamer.level_one_quotes(symbols=equities,fields= [0,1,2,3,4,5,6,7])
-  streamer.news_headline(symbols=equities,fields=[0,3,5,9])
+    fetch_instrument_data(instruments.keys())
 
-#include_default_counters()
-include_positions()
+def fetch_instrument_data(symbols:list):
+  """
+  Fetches list of instruments fundamental data
+  """
+  for symbol in symbols:
+    response = tdclient.search_instruments(
+      symbol=symbol,
+      projection='fundamental')
+    
+    send_service_data(
+      serviceName='fundamental',
+      contents=[response])
 
-streamer.write_flag=False
-streamer.stream(print_to_console=True)
+def send_service_data(serviceName:str, contents:list) -> None:
+  if serviceName is None:
+    raise ValueError('No serviceName provided')
+  if len(contents) == 0:
+    logger.warn('empty list given to send_service_data')
+    return
+
+  print(dumps({
+    'data':[
+      {
+        'service':serviceName,
+        'content':contents
+      }
+    ]
+  }))
+
+if __name__ == "__main__":
+  process_all_instruments()
+    
